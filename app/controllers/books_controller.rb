76fd5bd005ff8d57155b3ca3b68231cb38fc54a9
danainjax <danainjax@gmail.com>
2021-04-13 12:06:59 -0400

    added books controller new, create, show, and book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+       book = Book.new(book_params)
+        if book.save 
+            redirect_ to book_path(@book)
+        end
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+
+
+
+private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :published, :publisher, :description, :genre, :image, :link_to_purchase)
+    end
 end
