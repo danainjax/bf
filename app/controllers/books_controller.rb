4d14215ac2c91475073d0f017f3f5d915313618e
danainjax <danainjax@gmail.com>
2021-04-13 12:33:51 -0400

    added book views and partial, new, show, edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,20 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])
     end
 
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find_by_id(params[:id])
+        @book.update(book_params)
+        redirect_to book_path(@book)
+    end
+
+    def destroy
+        @book.destroy
+        redirect_to book_path(@book)
+    end
 
 
 
