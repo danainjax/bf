a2a67e75011fb64721384c4697c4ff14eb4fb2d6
danainjax <danainjax@gmail.com>
2021-04-13 13:16:59 -0400

    added review form, partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,44 @@
 class ReviewsController < ApplicationController
+    def new
+        @review = Review.new
+    end
+    
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+    
+    def create
+        @review = Review.new(review_params)
+        @review.save
+        redirect_to review_path(@review)
+    end
+    
+    def index
+        @reviews = Review.all 
+    end
+    
+    def edit
+        @review = Review.find_by_id(params[:id])
+    end
+    
+    def update
+        @review = Review.find_by_id(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+    
+    def destroy
+        @review = Review.find_by_id(params[:id])
+        @review.destroy
+    end
+    
+    private
+    
+        def review_params
+            params.require(:review).permit(:star_rating, :book_review, :status, :reader_id, :book_id, :id)
+                
+        end 
+    
+    
+    
 end
