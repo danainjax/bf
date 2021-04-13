5a972a31c70aad8768eb97e3c35163e178c7a932
danainjax <danainjax@gmail.com>
2021-04-13 13:36:20 -0400

    hooked up to NY Times Book Review API and put in book model. Call to seed database on the book model in seeds

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,16 @@
 class Book < ApplicationRecord
     has_many :reviews
     has_many :readers, through: :reviews  
+
+    def self.get_books
+        response = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{ENV["KEY"]}")
+        #byebug
+        books = JSON.parse(response)["results"]["books"]
+        #create a Ruby book object with all the info in the books array, use an iterator
+        books.each do |book|
+            self.create(title: book["title"], author: book["author"], publisher: book["publisher"], description: book["description"]) 
+        # binding.pry
+        
+        end
+    end
 end
