36fcaf1e3b625e69c070cb3ff40d907ff44122c5
danainjax <danainjax@gmail.com>
2021-04-17 09:55:55 -0400

    added index for reviews and added nested routes reviews under books

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
+
+  resources :readers 
   resources :reviews
-  resources :readers
-  resources :books
+
+  resources :books do
+    resources :reviews, only: [:index, :show, :new]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
