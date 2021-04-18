3ee3f67bd537681c0403436c9a478f0bba4c59b9
danainjax <danainjax@gmail.com>
2021-04-18 14:01:34 -0400

    updated api call to instantiate a book with the image url and link to purchase on Amazon

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ApplicationRecord
         books = JSON.parse(response)["results"]["books"]
         #create a Ruby book object with all the info in the books array, use an iterator
         books.each do |book|
-            self.create(title: book["title"], author: book["author"], publisher: book["publisher"], description: book["description"]) 
+            self.create(title: book["title"], author: book["author"], publisher: book["publisher"], description: book["description"], image: book["book_image"], link_to_purchase: book["amazon_product_url"]) 
         # binding.pry
         
         end
