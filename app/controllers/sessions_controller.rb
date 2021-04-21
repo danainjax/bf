53d93376d3a2384f0996d733b62f5fdb7bc49620
danainjax <danainjax@gmail.com>
2021-04-21 12:27:14 -0400

    login with google omniauth is tested and working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ class SessionsController < ApplicationController
   end
 
   def omniauth
-    # byebug
+    
     reader = Reader.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |r|
       r.username = request.env['omniauth.auth'][:info][:first_name]
       r.email = request.env['omniauth.auth'][:info][:email]
@@ -36,7 +36,7 @@ class SessionsController < ApplicationController
     
     if reader.valid?
       session[:reader_id] = reader.id
-      redirect_to root_path
+      redirect_to books_path
     else
       redirect_to login_path
     end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -5,7 +5,7 @@ class Reader < ApplicationRecord
 
     has_secure_password
 
-    validates :username, presence: true, uniqueness: true
-    validates :first_name, presence: true
-    validates :email, presence: true, uniqueness: true
+    # validates :username, presence: true, uniqueness: true
+    # validates :first_name, presence: true
+    # validates :email, presence: true, uniqueness: true
 end
