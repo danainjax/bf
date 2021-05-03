4d78dc2e4a242d00e34e71fd0c8d5f05c428b773
danainjax <danainjax@gmail.com>
2021-05-03 14:26:04 -0400

    added two more api calls for more book lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,5 +29,7 @@
 # Book.get_list("sports")
 # Book.get_list("science")
 # Book.get_list("religion-spirituality-and-faith")
-Book.get_list("family")
+# Book.get_list("family")
+Book.get_list("hardcover-political-books")
+Book.get_list("race-and-civil-rights")
 
