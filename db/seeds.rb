cec1069620b90864c3f5522f2fd7a3317671e139
danainjax <danainjax@gmail.com>
2021-05-12 11:18:50 -0400

    cformatted seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# do these requests max ten at a time, or will get a 429 error, too many requests
-# Book.get_list("hardcover-fiction")
-# Book.get_list("hardcover-nonfiction")
-# Book.get_list("young-adult")
-# Book.get_list("hardcover-advice")
-# Book.get_list("animals")
-# Book.get_list("celebrities")
-# Book.get_list("business-books")
-# Book.get_list("picture-books")
-# Book.get_list("combined-print-and-e-book-fiction")
+
+# do these requests max 9 -10  at a time, or will get a 429 error, too many requests
+Book.get_list("hardcover-fiction")
+Book.get_list("hardcover-nonfiction")
+Book.get_list("young-adult")
+Book.get_list("hardcover-advice")
+Book.get_list("animals")
+Book.get_list("celebrities")
+Book.get_list("business-books")
+Book.get_list("picture-books")
+Book.get_list("combined-print-and-e-book-fiction")
+
 # Book.get_list("combined-print-and-e-book-nonfiction")
 # Book.get_list("trade-fiction-paperback")
 # Book.get_list("mass-market-paperback")
@@ -24,12 +26,13 @@
 # Book.get_list("culture")
 # Book.get_list("food-and-fitness")
 # Book.get_list("humor")
+
 # Book.get_list("health")
 # Book.get_list("travel")
-Book.get_list("sports")
-Book.get_list("science")
-Book.get_list("religion-spirituality-and-faith")
-Book.get_list("family")
-Book.get_list("hardcover-political-books")
-Book.get_list("race-and-civil-rights")
+# Book.get_list("sports")
+# Book.get_list("science")
+# Book.get_list("religion-spirituality-and-faith")
+# Book.get_list("family")
+# Book.get_list("hardcover-political-books")
+# Book.get_list("race-and-civil-rights")
 
