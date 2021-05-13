03e665a3ba7ce1ceebce833d7c5644875ef4e0d2
danainjax <danainjax@gmail.com>
2021-05-13 11:12:02 -0400

    updated review validations to allow a status to be set without writing a review or rating the book, yet

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,8 @@ class Review < ApplicationRecord
     belongs_to :reader
 
     validates :status, presence: true
-    validates :book_review, length: { in: 5..500 }
-    validates :star_rating, :status, presence: true
+    validates :book_review, length: { in: 10..500 }, allow_blank: true
+    validates :star_rating, :status, presence: true, allow_blank: true
 
     scope :by_the_book, ->{order(:book_id)}
     scope :status, ->{order('status DESC')}
