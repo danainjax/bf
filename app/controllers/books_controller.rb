5cf2a173ebe77fa8b1ef12f0e3244e85c3b31917
danainjax <danainjax@gmail.com>
2021-05-13 10:57:19 -0400

    removed .all from class method call

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,7 +44,7 @@ class BooksController < ApplicationController
                 elsif params[:genre]
                     @books = Book.where('genre LIKE ?', "#{params[:genre]}")
             else
-                @books = Book.all.genre
+                @books = Book.genre
             end
        
     end
